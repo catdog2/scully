50b09cfef7d7d4308962329a50061608b0b01813
Markus Hauschild <hauschildm@gmx.net>
2013-05-31 19:04:25 +0200

    Add basic CMake buildsystem. Add the lemon parser (from the sqlite project).

---
+++ src/test.cpp
@@ -0,0 +1,9 @@
+/* The scully programming language.
+ *
+ * Copyright (c) Peter Dahlberg, Markus Hauschild and Florian Sattler, 2013.
+ * Licensed under the GNU GPL v2.
+ */
+
+int main() {
+    return 0;
+}
