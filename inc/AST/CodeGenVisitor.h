585a9bcc7446f4072caeb657218f84cea90b4bc2
Markus Hauschild <hauschildm@gmx.net>
2013-06-01 13:04:53 +0200

    Prepare CodeGenVisitor

---
+++ inc/AST/CodeGenVisitor.h
@@ -0,0 +1,31 @@
+#ifndef CODEGENVISITOR_H
+#define CODEGENVISITOR_H
+
+#include "ASTVisitor.h"
+#include <string>
+
+class CodeGenVisitor : public ASTVisitor {
+public:
+	CodeGenVisitor();
+	virtual ~CodeGenVisitor();
+
+ 	virtual void visit(AssignmentExpression* e);
+	virtual void visit(BinOpExpression* e);
+	virtual void visit(ConstantExpression* e);
+	virtual void visit(ExpressionStatement* e);
+	virtual void visit(ForStatement* e);
+	virtual void visit(FunctionCallExpression* e);
+	virtual void visit(FunctionDefinition* e);
+	virtual void visit(IfStatement* e);
+	virtual void visit(ParameterList* e);
+	virtual void visit(RandomForStatement* e);
+	virtual void visit(RandomIfStatement* e);
+	virtual void visit(ReturnStatement* e);
+	virtual void visit(Scope* e);
+	virtual void visit(StatementList* e);
+	virtual void visit(ValueList* e);
+	virtual void visit(VariableDefinition* e);
+private:
+};
+
+#endif // CODEGENVISITOR_H
---
+++ src/AST/CodeGenVisitor.cpp
@@ -0,0 +1,57 @@
+#include "AST/CodeGenVisitor.h"
+
+CodeGenVisitor::CodeGenVisitor() {
+	//
+}
+
+CodeGenVisitor::~CodeGenVisitor() {
+	//
+}
+
+void CodeGenVisitor::visit(AssignmentExpression* e) {
+}
+
+void CodeGenVisitor::visit(BinOpExpression* e) {
+}
+
+void CodeGenVisitor::visit(ConstantExpression* e) {
+}
+
+void CodeGenVisitor::visit(ExpressionStatement* e) {
+}
+
+void CodeGenVisitor::visit(ForStatement* e) {
+}
+
+void CodeGenVisitor::visit(FunctionCallExpression* e) {
+}
+
+void CodeGenVisitor::visit(FunctionDefinition* e) {
+}
+
+void CodeGenVisitor::visit(IfStatement* e) {
+}
+
+void CodeGenVisitor::visit(ParameterList* e) {
+}
+
+void CodeGenVisitor::visit(RandomForStatement* e) {
+}
+
+void CodeGenVisitor::visit(RandomIfStatement* e) {
+}
+
+void CodeGenVisitor::visit(ReturnStatement* e) {
+}
+
+void CodeGenVisitor::visit(Scope* e) {
+}
+
+void CodeGenVisitor::visit(StatementList* e) {
+}
+
+void CodeGenVisitor::visit(ValueList* e) {
+}
+
+void CodeGenVisitor::visit(VariableDefinition* e) {
+}
