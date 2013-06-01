cbf0feecf5d4d25f0626e65ea6945b69993a4a1c
Markus Hauschild <hauschildm@gmx.net>
2013-06-01 13:12:47 +0200

    Prepare for CodeGen (and use clang because it rocks!)

--- inc/AST/CodeGenVisitor.h
+++ inc/AST/CodeGenVisitor.h
@@ -2,6 +2,11 @@
 #define CODEGENVISITOR_H
 
 #include "ASTVisitor.h"
+#include "llvm/DerivedTypes.h"
+#include "llvm/IRBuilder.h"
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/Analysis/Verifier.h"
 #include <string>
 
 class CodeGenVisitor : public ASTVisitor {
